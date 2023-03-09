8e917e90f632de26260c8c52370220eae116a17a
david.huang <david.huang@ark88.net>
2023-03-09 16:42:03 +0800

    add google map in android app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,44 @@
-
 import 'package:flutter/material.dart';
-import 'login/login_screen.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
+void main() => runApp(const MyApp());
 
-void main() => runApp(const LoginScreen());
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(25.0339206, 121.5636985);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: Colors.green[700],
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Maps Sample App'),
+          elevation: 2,
+        ),
+        body: GoogleMap(
+          zoomGesturesEnabled: true,
+          onMapCreated: _onMapCreated,
+          mapType: MapType.normal,
+          compassEnabled: true,
+          initialCameraPosition: CameraPosition(target: _center, zoom: 11.0, bearing: 30, tilt: 50),
+        ),
+      ),
+    );
+  }
+}
